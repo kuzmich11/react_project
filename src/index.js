f2953023dc0437343f7a28c9e1980f570d05fdba
kuzmich11 <v.a.kuznetsov-83@yandex.ru>
2022-01-15 15:16:01 +0300

    ДЗ к уроку 1

--- src/index.js
+++ src/index.js
@@ -2,14 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Message from './Message';
 import reportWebVitals from './reportWebVitals';
+
+const myName = 'Vladimir'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App name={myName} />
+     
   </React.StrictMode>,
   document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
